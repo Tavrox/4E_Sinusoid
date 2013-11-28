9f34ca032e3326d824325003408fd90bf7082571
Tavrox <tavrox@gmail.com>
2013-11-28 14:33:53 +0100

    Clean enemy cs

--- Assets/scripts/Gameplay/Characters/Enemy.cs
+++ Assets/scripts/Gameplay/Characters/Enemy.cs
@@ -3,13 +3,5 @@ using System.Collections;
 
 public class Enemy : Character {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
