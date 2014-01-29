7916cf540a4e7ed551f1975199f7214423b01ccd
Tavrox <tavrox@gmail.com>
2014-01-29 11:53:44 +0100

    Forgot the print lol :)

--- Assets/scripts/Gameplay/Characters/PlayerAnims.cs
+++ Assets/scripts/Gameplay/Characters/PlayerAnims.cs
@@ -89,11 +89,6 @@ public class PlayerAnims : MonoBehaviour
 	}
 	private void Stand()
 	{	
-		print ("CHECK RIGHT" + _character.isRight);
-		print ("CHECK GROUND" + _character.grounded);
-		print ("CHECK ANIM " + currentAnim);
-		print ("CHECK FACING" + _character.facingDir);
-		print ("CHECK PLAYING" + animPlaying);
 		if(!_character.isLeft && _character.grounded == true && currentAnim != animDef.StandLeft && _character.facingDir == Character.facing.Left && animPlaying == false)
 		{
 			currentAnim = animDef.StandLeft;
